8da65de723f9813f4cd5fde8423d6d26f837f091
Chezter Reblando <clc.reblando@gmail.com>
2024-09-24 19:05:53 +0800

    day-2

--- Day-2/Exercise1/convertedHours.java
+++ Day-2/Exercise1/convertedHours.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class convertedHours{
     public static void main(String[] args){
-        
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the number of minutes: ");
+        int minutes= input.nextInt();
+
+        int hours= minutes/60;
+        int convertedMinutes= minutes%60;
+        System.out.println("Converted hours: "+ hours + " hours and "+ convertedMinutes+ " minute/s");
     }
 }
