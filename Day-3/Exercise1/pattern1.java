be6c6d4c33a9d2d90f6a9c18ee3ce969fd3bb1fe
superchezzz <clc.reblando@gmail.com>
2024-09-26 11:50:17 +0800

    day-3

---
+++ Day-3/Exercise1/pattern1.java
@@ -0,0 +1,10 @@
+public class pattern1{
+    public static void main(String[] args){
+        for(int x=1; x<=5; x++){
+            for(int y=0; y<x; y++){
+                System.out.print(y+1);
+            }
+            System.out.println();
+        }
+    }
+}
